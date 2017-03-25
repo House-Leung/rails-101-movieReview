5004e9d1e7cab66babb5d2742423e6fc7ccdb25f
House_Leung <House.liangshihao@gmail.com>
2017-03-26 01:58:57 +0800

    replace root with movies#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :movies
 
   get "about/index"
-  root 'about#index'
+  root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

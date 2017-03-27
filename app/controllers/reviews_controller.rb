f9eca66a3cc9ed36228e49b85eb73799ed9bbba1
House_Leung <House.liangshihao@gmail.com>
2017-03-27 19:54:31 +0800

    implement reviews#edit/update & reviews#destroy action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,6 +20,32 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:movie_id])
+    @review = review.find(params[:id])
+    @review = Review.update(review_params)
+    @review.movie = @movie
+    @review.user = current_user
+
+    if @review.save
+      redirect_to account_reviews_path, notice: "电影评论修改成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+      redirect_to account_reviews_path, alert: "已删除电影评论！"
+  end
+
   private
 
   def review_params

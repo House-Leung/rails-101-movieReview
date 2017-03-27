c94393fefdceb6c200d1410968ccf077188aafe0
House_Leung <House.liangshihao@gmail.com>
2017-03-27 20:47:53 +0800

    people can review only after  favorite movie

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,13 @@
 class ReviewsController < ApplicationController
 
   before_action :authenticate_user!, :only => [:new, :create]
+  before_action :find_movie_and_check_favorite_permission
 
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = Review.new
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = Review.new(review_params)
     @review.movie = @movie
     @review.user = current_user
@@ -21,12 +20,10 @@ class ReviewsController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params[:movie_id])
     @review = Review.find(params[:id])
   end
 
   def update
-    @movie = Movie.find(params[:movie_id])
     @review = review.find(params[:id])
     @review = Review.update(review_params)
     @review.movie = @movie
@@ -40,13 +37,19 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params[:movie_id])
     @review = Review.find(params[:id])
     @review.destroy
       redirect_to account_reviews_path, alert: "已删除电影评论！"
   end
 
   private
+
+  def find_movie_and_check_favorite_permission
+    @movie = Movie.find(params[:movie_id])
+    if !current_user.is_member_of?(@movie)
+      redirect_to movie_path(@movie), alert: "你需要收藏该电影后才能发表评论！"
+    end
+  end
 
   def review_params
     params.require(:review).permit(:content)

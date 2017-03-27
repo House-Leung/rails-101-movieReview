39b7d12282f8e12585ad5eff86423c655899e68e
House_Leung <House.liangshihao@gmail.com>
2017-03-27 19:25:35 +0800

    implment my favorite movies

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  namespace :account do
+    resources :movies 
+  end
+
   get "about/index"
   root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
